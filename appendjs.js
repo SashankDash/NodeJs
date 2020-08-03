a5891731e74d986f2dc4d48b13512a0b926aa886
Sashank <sashanksekharbapun@gmail.com>
2020-08-03 19:31:09 +0530

    3

---
+++ appendjs.js
@@ -0,0 +1,7 @@
+const fs = require('fs')
+
+//fs.writeFileSync('notes.txt','This is my first write in node js')
+
+//This code is to append in nodejs.
+
+fs.appendFileSync('notes.txt', ' this is my first append');
