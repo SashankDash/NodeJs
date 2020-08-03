2ef800f21b625ca34ff0cc1e9f2e86a0bb5c01b2
Sashank <sashanksekharbapun@gmail.com>
2020-08-03 19:04:41 +0530

    2nd commit

--- noblocking.js
+++ noblocking.js
@@ -1,11 +1,13 @@
 //nonblocking
-fs.readFile('text.txt',function(err,data){
+ var fs = require("fs");
+
+ fs.readFile('text.txt',function(err,data){
     if(err){
         console.log(err);
 
     }
     setTimeout(()=>{
-        console.log("Display After 2 Second")
-    },2000);
+        console.log("after 5 seconds")
+    },5000);
 })
 console.log('Starting here');
